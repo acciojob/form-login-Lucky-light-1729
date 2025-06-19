1feb04dcadfefa97cda2062fd0095ce408f21653
Anay Agrawal <agrawalanay20@gmail.com>
2025-06-19 13:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+	event.preventDefault(); 
+	
+	const firstName = document.querySelector('[name="fname"]').value.trim();
+	const lastName = document.querySelector('[name="lname"]').value.trim();
+	
+	const fullName = `${firstName} ${lastName}`.trim();
+	
+	alert(fullName);
 
 }
